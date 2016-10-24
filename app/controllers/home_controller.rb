844baf7e6eb064ea2d5d716670d015c2ef403dea
Christy <cvalette@lucca.fr>
2016-10-24 18:08:41 +0000

    exo semaine 3

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,15 @@
 class HomeController < ApplicationController
   def index
+    @name = 'Christy'
+    @surname_1 = 'Tate'
+    @surname_2 = 'Maluca'
+    @surname_3 = 'Chat méchant'
+    @comment = 'Elle est au top'
+    
+    @curiosities = [
+      'surf',
+      'chill',
+      'troll']
+    
   end
 end
